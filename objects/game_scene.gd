c0224332b3a0a5ad939ccedef233576145332700
Jimmy <nosemeocurrenada93@gmail.com>
2023-02-04 19:32:48 -0300

    Add ESC for exit

--- objects/game_scene.gd
+++ objects/game_scene.gd
@@ -11,6 +11,10 @@ func _cutscene():
 	pass
 
 
+func _process(delta):
+	if Input.is_action_pressed("ui_cancel"):
+		get_tree().quit()
+
 func dialog(character: String, translation_key: String) -> Dialog:
 	if canvas_layer == null:
 		canvas_layer = CanvasLayer.new()
