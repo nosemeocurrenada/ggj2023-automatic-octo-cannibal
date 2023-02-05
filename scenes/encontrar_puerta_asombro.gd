18b46d7e1ad0b64e2d8717dc7a068a2058bf39b5
Jimmy <nosemeocurrenada93@gmail.com>
2023-02-04 21:45:00 -0300

    Add door scene for second minigame

---
+++ scenes/encontrar_puerta_asombro.gd
@@ -0,0 +1,11 @@
+extends GameScene
+
+
+func _cutscene():
+	$backdoors/door.queue_free()
+	$backdoors/door3.queue_free()
+
+
+func _on_portal_player_entered():
+	yield(dialog('big_chef', 'ARE YOU READY FOR A JUEGAZO?'), 'done')
+
