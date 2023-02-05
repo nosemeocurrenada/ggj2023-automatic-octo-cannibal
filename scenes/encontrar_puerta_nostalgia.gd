5e03af532f2b2ae8974bccd27a5f51c38c5f0518
Melissa Kuris <melissakuris@hotmail.com>
2023-02-04 22:53:44 -0300

    Consigue Nostalgia + Entrega Nostalgia

--- scenes/encontrar_puerta_nostalgia.gd
+++ scenes/encontrar_puerta_nostalgia.gd
@@ -8,3 +8,11 @@ func _cutscene():
 func _on_portal_player_entered():
 	yield(dialog('big_chef', 'ARE YOU READY FOR A JUEGAZO?'), 'done')
 
+
+
+func _on_closed_door_2_player_entered():
+	yield(dialog('dialog_box', 'Este recuerdo todavía no está disponible'), 'done')
+
+
+func _on_closed_door_3_player_entered():
+	yield(dialog('dialog_box', 'Este recuerdo todavía no está disponible'), 'done')
