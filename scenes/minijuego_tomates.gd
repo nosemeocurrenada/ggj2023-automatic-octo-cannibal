08f72e59c9ee6db2472c274df2f10f14d1475a45
nosemeocurrenada <nosemeocurrenada93@gmail.com>
2023-02-05 22:43:00 -0300

    fix tomates

--- scenes/minijuego_tomates.gd
+++ scenes/minijuego_tomates.gd
@@ -2,9 +2,10 @@ extends GameScene
 
 const tomato_scene = preload("res://objects/games/tomates/tomato.tscn")
 const velocity = 200
-export var GOAL = 10
+export var GOAL = 2
 var rng = RandomNumberGenerator.new()
 var score = 0
+var game_finished = false # por algun motivo no funcionaba desde _process
 
 
 func _cutscene():
@@ -18,13 +19,15 @@ func _process(delta):
 		direction = Vector2.LEFT
 	if Input.is_action_pressed("ui_right"):
 		direction = Vector2.RIGHT
+	if Input.is_action_just_pressed("ui_accept"):
+		_end_game()
 	$player.move_and_slide(direction.normalized() * velocity)
 
 	$score.text = '%s' % (GOAL - score)
 	
-	if GOAL <= score:		
-		get_tree().change_scene("res://scenes/consigue_ingrediente_Nostalgia.tscn")
-	
+#	if GOAL <= score:
+#		_end_game()
+
 
 func _create_tomato():
 	var instance = tomato_scene.instance()
@@ -33,12 +36,15 @@ func _create_tomato():
 	instance.position.y = rng.randi_range($spawn_min.position.y, $spawn_max.position.y)
 	instance.contact_monitor = true
 	instance.contacts_reported = 3
+	instance.add_to_group('tomato')
 	# connect(signal: String, target: Object, method: String, binds: Array = [  ], flags: int = 0)
 	instance.connect("tomato_catch", self, '_on_tomate_body_entered')
 	self.add_child(instance)
 
 
 func _on_tomate_body_entered(body):
+	if game_finished:
+		return
 	body.queue_free()
 	score += 1
 	rng.randomize()
@@ -49,5 +55,20 @@ func _on_tomate_body_entered(body):
 
 
 func _on_timer_timeout():
-	_create_tomato()
-	$timer.wait_time *= .97
+	if GOAL > score:
+		_create_tomato()
+		$timer.wait_time *= .97
+	elif not game_finished:
+		game_finished = true
+		_end_game()
+
+
+func _end_game():
+	for tom in get_tree().get_nodes_in_group("tomato"):
+		tom.gravity_scale = -.1
+		$tween.interpolate_property(tom, 'modulate:a', 1, 0, 2)
+	$tween.start()
+
+
+func _on_tween_tween_all_completed():
+	get_tree().change_scene("res://scenes/consigue_ingrediente_Nostalgia.tscn")	
